cbb46e31178eac9b88e5a62f433754a6d369cc3e
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-06-05 19:34:02 -0300

    global style file

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">home</a>
+                <a href="/">Home</a>
                 <a href="/create">New Blog</a>
             </div>
         </nav>
