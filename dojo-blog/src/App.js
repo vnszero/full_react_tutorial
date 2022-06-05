80c8c88e23317625b5af22a0745959a3951507f6
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-06-05 16:12:51 -0300

    nested components

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "http://www.google.com";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        <p>{ Math.random()*10 }</p>
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );
