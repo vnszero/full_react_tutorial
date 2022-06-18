56666fa5147c01155a0eb9793235d262731ed1ad
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-06-18 19:49:16 -0300

    onClick

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('Hello Ninjas',e);
+    }
+    const handleClickAgain = (name) => {
+        console.log('Hello '+name)
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={()=>handleClickAgain('Mario')}>Click Me Again</button>
         </div>
     );
 }
