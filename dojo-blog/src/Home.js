f6af13b3513bd4e1fedd86f38ca835a713842730
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-06-20 16:32:42 -0300

    useEffect

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
-
 const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: "my first blog", body: "lorem ipsum...", author: "Mario", id: 1},
         {title: "my web site", body: "lorem ipsum...", author: "Luigi", id: 2},
         {title: "New game", body: "lorem ipsum...", author: "Wario", id: 3}
     ])
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log({blogs});
+    })
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All titles!" setBlogs={setBlogs}/>
