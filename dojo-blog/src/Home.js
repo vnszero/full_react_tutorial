4ca0dcdae0dc1f4d4d083791015dcb61d453fbf8
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-06-20 17:28:57 -0300

    fetching data

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: "my first blog", body: "lorem ipsum...", author: "Mario", id: 1},
-        {title: "my web site", body: "lorem ipsum...", author: "Luigi", id: 2},
-        {title: "New game", body: "lorem ipsum...", author: "Wario", id: 3}
-    ])
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('use effect ran');
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
     },[]);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All titles!" setBlogs={setBlogs}/>
+            {blogs && <BlogList blogs={blogs} title="All titles!" setBlogs={setBlogs}/>}
         </div>
     );
 }
