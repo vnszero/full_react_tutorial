cab490f9772a7095b0fd9a2573e4b0b9f88eeff3
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-06-20 21:52:29 -0300

    error catcher

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,20 +3,29 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true)
-
+    const [error, setError] = useState(null)
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok){
+                    throw Error('Could not fetch the data for server');
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
             })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            });
     },[]);
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div> }
             {isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All titles!" setBlogs={setBlogs}/>}
         </div>
