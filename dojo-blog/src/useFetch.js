4fb5157f46e6453ed7c8117f97c977711c673649
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-06-21 17:11:19 -0300

    useEffect Cleanup

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -4,7 +4,9 @@ const useFetch = (url) => {
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController();
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if(!res.ok){
                     throw Error('Could not fetch the data for server');
@@ -17,9 +19,15 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
-                setIsPending(false);
-                setError(err.message);
+                if(err.name === 'AbortError'){
+                    console.log('fetch aborted');
+                } else {
+                    setIsPending(false);
+                    setError(err.message);
+                }
             });
+        }, 1000);
+        return () => abortCont.abort();
     },[url]);
 
     return { data, isPending, error };
