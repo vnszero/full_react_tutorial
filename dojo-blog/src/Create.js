353dd1425e3014bc5f99f219a42d204e246c8ab9
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-06-22 11:20:20 -0300

    Controlled Inputs

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,41 @@
+import { useState } from 'react';
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario');
+
     return (
         <div className="create">
             <h2>Create a New Blog</h2>
+            <form>
+                <label>Title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Body:</label>
+                <input 
+                    type="text"
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+                <label>Author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Mario">Mario</option>
+                    <option value="Yoshi">Yoshi</option>
+                    <option value="Luigi">Luigi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
     );
 }
